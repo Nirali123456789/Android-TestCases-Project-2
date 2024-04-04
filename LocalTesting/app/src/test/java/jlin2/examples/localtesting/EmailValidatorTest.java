be8ae0293cf7be6a88e4adc88dcf968fbedc31b9
Kristina Papko <papko.kristina.qa.com>
2024-04-03 22:10:33 -0400

    added negative test in EmailValidatorTest

--- LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.java
+++ LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.java
@@ -2,6 +2,7 @@ package jlin2.examples.localtesting;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class EmailValidatorTest {
@@ -13,6 +14,13 @@ public class EmailValidatorTest {
         assertTrue(EmailValidator.Companion.isValidEmail("22@viranch.co.ca"));
         assertTrue(EmailValidator.Companion.isValidEmail("nehavaidya@viranch.com"));
         assertTrue(EmailValidator.Companion.isValidEmail("n_pandya261091@fanshaweonline.ca"));
+    }
+
+    @Test
+    public void testInvalidEmail() {
+        assertFalse(EmailValidator.Companion.isValidEmail("88@abc"));
+        assertFalse(EmailValidator.Companion.isValidEmail(""));
+        assertFalse(EmailValidator.Companion.isValidEmail(null));
     }
 
 
